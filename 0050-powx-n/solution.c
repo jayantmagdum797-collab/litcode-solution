f26d87eee070ce90a751acb7e78c7adc060f62bf
jayantmagdum797-collab <jayantmagdum797@gmail.com>
2026-07-29 11:29:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (80.30%)

---
+++ 0050-powx-n/solution.c
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+    int i;
+    long long a = n;
+    double result = 1.0;
+    if(n<0)
+    {
+        x = 1.0 / x;
+        a = -(a);
+    }
+        while(a>0)
+        {
+            if(a % 2 == 1)
+            result= result * x;   
+             x = x*x;
+             a = a /2;       
+        }
+       return result;
+}
+
